930535b85ab4841e6a6233fa77c6b919151a4168
Raj Tharun <tharunjasti@gmail.com>
2018-01-23 17:46:55 -0500

    working with angularjs and rest services

--- aMusicStore/src/main/webapp/WEB-INF/resources/js/controller.js
+++ aMusicStore/src/main/webapp/WEB-INF/resources/js/controller.js
@@ -7,13 +7,13 @@ var cartApp = angular.module ("cartApp", []);
 cartApp.controller("cartCtrl", function ($scope, $http){
 
     $scope.refreshCart = function (cartId) {
-        $http.get('/eMusicStore/rest/cart/'+$scope.cartId).success(function (data) {
+        $http.get('/aMusicStore/rest/cart/'+$scope.cartId).success(function (data) {
            $scope.cart=data;
         });
     };
 
     $scope.clearCart = function () {
-        $http.delete('/eMusicStore/rest/cart/'+$scope.cartId).success($scope.refreshCart($scope.cartId));
+        $http.delete('/aMusicStore/rest/cart/'+$scope.cartId).success($scope.refreshCart($scope.cartId));
     };
 
     $scope.initCartId = function (cartId) {
@@ -22,15 +22,15 @@ cartApp.controller("cartCtrl", function ($scope, $http){
     };
 
     $scope.addToCart = function (productId) {
-        $http.put('/eMusicStore/rest/cart/add/'+productId).success(function (data) {
-            $scope.refreshCart($http.get('/eMusicStore/rest/cart/cartId'));
+        $http.put('/aMusicStore/rest/cart/add/'+productId).success(function (data) {
+            $scope.refreshCart($http.get('/aMusicStore/rest/cart/cartId'));
             alert("Product successfully added to the cart!")
         });
     };
 
     $scope.removeFromCart = function (productId) {
-        $http.put('/eMusicStore/rest/cart/remove/'+productId).success(function (data) {
-            $scope.refreshCart($http.get('/eMusicStore/rest/cart/cartId'));
+        $http.put('/aMusicStore/rest/cart/remove/'+productId).success(function (data) {
+            $scope.refreshCart($http.get('/aMusicStore/rest/cart/cartId'));
         });
     };
 });
